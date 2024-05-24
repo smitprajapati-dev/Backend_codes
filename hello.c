9725fc5d24bf392f074b00beabd0bb29810b0f24
smit prajapati <smitprajapati002@gmail.com>
2024-05-24 17:43:48 -0600

    commited some c and c++ codes

--- hello.c
+++ hello.c
@@ -1,6 +1,19 @@
+// #include<stdio.h>
+
+// int main(){
+//     printf("Smit prajapati");
+//     return 0;
+// }
+
 #include<stdio.h>
 
 int main(){
-    printf("Smit prajapati");
+    int a=12;
+    float f = 12.1;
+    double d = 123.2;
+    // printf(a);
+
     return 0;
+
+
 }
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // prepocessor command
+
+using namespace std;
+
+int main(){
+    cout<<"Smit Prajapati"<<endl;
+    cout<<"Next Line";
+    int a , b,c ;
+    a = 2;
+    b = 21;
+    c = 13;
+    char j = 'h';
+    string stn = "Smit Prajapati";
+    long m = 12;    
+    cout<<stn<<m<<stn; 
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    return 0;
+}
