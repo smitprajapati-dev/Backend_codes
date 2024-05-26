a72db307e46fb164d050f86c1cb7debb3a92959b
smit prajapati <smitprajapati002@gmail.com>
2024-05-25 18:34:33 -0600

    commited some solution of febonacchi series

--- main.cpp
+++ main.cpp
@@ -3,18 +3,76 @@
 using namespace std;
 
 int main(){
-    cout<<"Smit Prajapati"<<endl;
-    cout<<"Next Line";
-    int a , b,c ;
-    a = 2;
-    b = 21;
-    c = 13;
-    char j = 'h';
-    string stn = "Smit Prajapati";
-    long m = 12;    
-    cout<<stn<<m<<stn; 
-    cout<<a<<endl;
-    cout<<b<<endl;
-    cout<<c<<endl;
+    // cout<<"Smit Prajapati"<<endl;
+    // cout<<"Next Line";
+    // int a , b,c ;
+    // a = 2;
+    // b = 21;
+    // c = 13;
+    // char j = 'h';
+    // string stn = "Smit Prajapati";
+    // long m = 12;    
+    // cout<<stn<<m<<stn; 
+    // cout<<a<<endl;
+    // cout<<b<<endl;
+    // cout<<c<<endl;
+    // int const a = 12;
+    // cout<<a;
+    // Calculator logic
+
+    // char op;
+    // float n1,n2;
+    // cout<<"Please enter which iteration you want";
+    // cin>>op;
+    // cout<<"Please enter n1 and n2";
+    // cin>>n1>>n2;
+    // switch(op){
+    //     case '+':
+    //         cout<<n1 <<"+"<<n2<< "="<< n1 + n2;
+    //         break;
+    //     case '-':
+    //         cout<<n1 << '-' <<n2 << "=" << n1 - n2;
+    //         break;
+    //     case '*':
+    //         cout <<n1 <<'*' << n2 << "=" << n1 * n2;
+    //         break;
+    //     case '/':
+    //         cout <<n1 <<'/' << n2 << "=" << n1 / n2;
+    //         break;
+    //     default:
+    //         cout<< "Operator is not correct";
+    //         break;
+    //}
+    // int n,t1 =0, t2 = 1, nextTerm = 0;
+    // cout<<"Enter the number of terms : ";
+    // cin >> n;
+    // cout<<"Fibonacchi Series";
+    // for(int i;i<=n; ++i){
+    //     if(i == 1){
+    //         cout<<t1 <<", ";
+    //         continue;
+    //     }
+    //     if(i == 1){
+    //         cout<<t2 << ", ";
+    //         continue;
+    //     }
+    //     nextTerm = t1 + t2;
+    //     t1 = t2;
+    //     t2 = nextTerm;
+    //     cout<<nextTerm<<", ";
+    // }
+    int n,t1 =0, t2 = 1, nextTerm = 0;
+    cout<<"Enter the number of terms: ";
+    cin>>n;
+    
+    cout<< "Fibonachi Series: " << t1<< ", "<<t2 << ", ";
+    nextTerm = t1 + t2;
+    while(nextTerm<=n){
+        cout<<nextTerm<< ", ";
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+        
+    }
     return 0;
 }
